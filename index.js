1f2558f609699d0f6a2313aca06a8020b939b4da
Rahul <cs.rajat75@gmail.com>
2025-08-29 01:50:30 +0600

    call and coin use section

--- index.js
+++ index.js
@@ -10,3 +10,31 @@ for(const icon of heartIcons){
     })
 }
 
+// call and coin use section
+
+let coin=100;
+
+const coinCount = document.getElementById('coin-count');
+const callBtn = document.querySelectorAll('.call-btn');
+
+for(const button of callBtn){
+    button.addEventListener('click', function(){
+        
+    const card = button.closest('.card');
+    const serviceName= card.querySelector('h1').innerText;
+
+    const serviceNumber= card.querySelector('.number').innerText;
+
+    if(coin<20){
+        alert('Not enough to make a call');
+        return;     
+    }
+    alert(`Calling ${serviceName} -${serviceNumber}`);
+    coin -= 20;
+    coinCount.innerText=coin;
+    
+    })
+};
+
+
+
