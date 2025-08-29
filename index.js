00c5cc56b5f7516a9e9ed2925b3b971b40751b23
Rahul <cs.rajat75@gmail.com>
2025-08-29 11:55:14 +0600

    Call History

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ for(const icon of heartIcons){
     })
 }
 
-// call and coin use section
+// Call History Section and coin use section
 
 let coin=100;
 
 const coinCount = document.getElementById('coin-count');
 const callBtn = document.querySelectorAll('.call-btn');
+const history = document.querySelector('.history-box');
 
 for(const button of callBtn){
     button.addEventListener('click', function(){
@@ -33,8 +34,29 @@ for(const button of callBtn){
     coin -= 20;
     coinCount.innerText=coin;
     
-    })
-};
+    // Call History
+
+    const date =new Date();
+    const time = date.toLocaleTimeString();
+
+    const newHistory = document.createElement('div');
+
+    newHistory.innerHTML=`
+        <div class="flex justify-between items-center bg-[rgba(250,250,250,1)] rounded-lg mt-2 p-2">
+       <div>
+        <p class="font-bold">${serviceName}</p>
+        <p class="text-gray-500">${serviceNumber}</p>
+      </div>
+      <div>
+        <p>${time}</p>
+      </div>
+      </div>
+    `;
+
+    history.appendChild(newHistory);
+    
+    });
+}
 
 
 
